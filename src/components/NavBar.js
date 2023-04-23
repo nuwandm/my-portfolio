f3110d5af6bd9258bdffa9263d89a30d942f084b
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2023-04-23 23:22:43 +0530

    Added Custom Links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import Logo from "./Logo";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  return (
+    <Link href={href} className={`${className}`}>
+      {title}
+    </Link>
+  );
+};
 
 const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/project">Projects</Link>
-        <Link href="/article">Articles</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/project" title="Projects" className="mx-4" />
+        <CustomLink href="/article" title="Articles" className="ml-4" />
       </nav>
-      <h2>Logo</h2>
+
       <nav>
+        {/* target={"_blank"} will open this link in new tab  */}
         <Link href="/" target={"_blank"}>
           Twitter
         </Link>
@@ -25,6 +35,9 @@ const NavBar = () => {
           Blog
         </Link>
       </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
+import Head from "next/head";
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -9,10 +10,10 @@ const montserrat = Montserrat({
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <head>
+      <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-      </head>
+      </Head>
       <main
         className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
       >

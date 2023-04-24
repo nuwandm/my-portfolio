8a6ea7f0a088c41ac9e9c2f98a4a235e6de5d97f
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2023-04-24 10:26:57 +0530

    Added Social meadea icons (svgr)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import Logo from "./Logo";
+import { useRouter } from "next/router";
+import { TwitterIcon, GithubIcon, LinkedInIcon, FaceBookIcon } from "./Icons";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  //   console.log(router); check what provides by router in the console
   return (
-    <Link href={href} className={`${className}`}>
+    <Link href={href} className={`${className} relative group`}>
       {title}
+      <span
+        className={`h-[1px] inline-block  bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+        ${router.asPath === href ? "w-full" : "w-0"}
+        `}
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
@@ -20,19 +31,19 @@ const NavBar = () => {
         <CustomLink href="/article" title="Articles" className="ml-4" />
       </nav>
 
-      <nav>
+      <nav className="flex items-center justify-center flex-wrap">
         {/* target={"_blank"} will open this link in new tab  */}
         <Link href="/" target={"_blank"}>
-          Twitter
+          <TwitterIcon />
         </Link>
         <Link href="/" target={"_blank"}>
-          Fb
+          <GithubIcon />
         </Link>
         <Link href="/" target={"_blank"}>
-          Linkdln
+          <LinkedInIcon />
         </Link>
         <Link href="/" target={"_blank"}>
-          Blog
+          <FaceBookIcon />
         </Link>
       </nav>
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">

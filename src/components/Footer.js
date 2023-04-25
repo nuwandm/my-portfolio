a3bbe23185b3360fc1a15223c02754359845dbc7
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2023-04-25 13:10:39 +0530

    Created footers and finished home page..❤️

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import Layout from "./Layout";
+import { useState, useEffect } from "react";
+
+const Footer = () => {
+
+  return (
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+      <Layout className="py-8 flex items-center justify-between">
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+        <div className="flex items-center">
+          Bulild By&nbsp;
+          <span className="text-primary" text-2xl px-1>
+            &#9825; &nbsp;
+          </span>
+          <Link
+            href="https://www.linkedin.com/in/nuwanmadusanka/"
+            className="underline underline-offset-2"
+            target="{_blank}"
+          >
+            Nuwan Madusanka
+          </Link>
+        </div>
+        
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;

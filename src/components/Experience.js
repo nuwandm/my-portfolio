9297c9d003f268710d0d609325b0f4b90bf3293e
Nuwan Madusanka <56627412+nuwandm@users.noreply.github.com>
2023-09-07 20:10:33 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -57,11 +57,11 @@ const Experience = () => {
             work=" I am an Associate Software Engineer with a strong proficiency in the MERN stack (MongoDB, Express.js, React, and Node.js). With a passion for building scalable and user-friendly web applications, I strive to deliver innovative solutions that meet both technical and business requirements. With a solid foundation in front-end and back-end development, I excel in creating responsive UIs, implementing efficient APIs, and ensuring seamless integration between the different components of a project. I am eager to contribute my skills and knowledge to impactful projects and collaborate with talented teams to drive digital innovation. Explore my portfolio to see examples of my work and get in touch to discuss how I can help bring your ideas to life."
           />
           <Details
-            position=" Associate Software Engineer"
+            position=" Trainee Software Engineer"
             company="SmartCode"
             address=" Negombo
 Sri Lanka."
-            time="2022-present"
+            time="2022-2023"
             work="My main responsibility is overseeing the implementation of an ERP system for an apparel garment factory. This includes developing the front-end user interface of the ERP system using React, HTML, CSS, and JavaScript, as well as developing the server-side back-end logic of the ERP system using ASP.NET. In addition, I am also responsible for implementing Microsoft SQL Server as the database engine. However, my role does not stop at developing the system. I am also collaborating with other development team members and working closely with the client garment factory stakeholders to ensure that the ERP system meets the requirements and goals of the organization."
           />
           <Details

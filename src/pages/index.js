d37c89a3644e4711d2d9030adaa4d33c8b1f625c
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2024-09-14 22:23:18 +0530

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,9 +46,10 @@ export default function Home() {
 								that exceed client expectations. Let's create something amazing
 								together!
 							</p>
+
 							<div className="flex items-center self-start mt-2 lg:self-center">
 								<Link
-									href="/Nuwan_Madusanka.pdf"
+									href="/Resume/Nuwan_Madusanka_Resume.pdf"
 									target={"_blank"}
 									className="flex items-center bg-dark text-light p-2.5 px-6
                 rounded-lg text-lg font-semibold  hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark

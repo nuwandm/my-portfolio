5e7e991ff82d9ed0e89a51656f7f2ec53d38c3ee
Nuwan Madusanka <56627412+nuwandm@users.noreply.github.com>
2025-05-17 13:34:37 +0530

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_ACTIONS || false;
-
 const nextConfig = {
   output: "export",
-  assetPrefix: isGitHubPages ? "/my-portfolio/" : "",
-  basePath: isGitHubPages ? "/my-portfolio" : "",
-  trailingSlash: true, 
+  basePath: "/my-portfolio",
+  assetPrefix: "/my-portfolio/",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;

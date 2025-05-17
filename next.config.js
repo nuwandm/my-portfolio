4e0b1e200a062a0a7fc42986ed585a87785745d0
Nuwan Madusanka <56627412+nuwandm@users.noreply.github.com>
2025-05-17 13:13:01 +0530

    cleanup

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
-module.exports = {
-  output: 'export',
-  basePath: isGithubPages ? '/my-portfolio' : '',
-  assetPrefix: isGithubPages ? '/my-portfolio/' : '',
-};

a16e0a46a6ca7d14a0dce2036f2cff02191c40d7
Nuwan Madusanka <56627412+nuwandm@users.noreply.github.com>
2025-05-17 12:22:38 +0530

    changes of next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // If your repo name is not username.github.io, uncomment and adjust this:
-  // basePath: '/your-repo-name',
+  basePath: "/my-portfolio",
   images: {
     unoptimized: true,
   },
